617690f74ec4732007324a419a234b6ccb34e7e5
Matt <matteo.dohnalek@gmx.de>
2025-03-16 18:52:52 +0100

    removed unnessasary files

--- old-recruitment-backup.py
+++
@@ -1,6 +0,0 @@
-
-### Write Somthing in the activity channel
-activity_channel = guild.get_channel(ACTIVITY_CHANNEL_ID)
-if activity_channel:
-    await activity_channel.send(f"❌ {ign} tried to apply for a closed region: {region}")
-    embed = discord.Embed(title=f"{interaction.user.name}] tried to apply for REGION!", description=f"{interaction.user.mention} tried to apply for a closed region!", colour=0xcfd746)
